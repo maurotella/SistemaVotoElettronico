5c39eb1329bf4d699aabf728b1d4c5007e1f5a26
Mauro <maurotellaroli@gmail.com>
2022-09-04 18:23:06 +0200

    Reso più carino il login grafico (PS: bisogna installare ikonli per le icone)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,10 @@
 import javafx.application.Application;
-import javafx.*;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
-import java.util.*;
+import java.util.Objects;
 
 public class App extends Application {
     @Override
@@ -25,4 +23,5 @@ public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
 }
