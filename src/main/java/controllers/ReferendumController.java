7dcaeadd552baa2c2ca620bf12965dc6f8ac1dad
Mauro <maurotellaroli@gmail.com>
2022-09-06 00:38:15 +0200

    Modifiche Diagramma sequenza uml

---
+++ src/main/java/controllers/ReferendumController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+
+public class ReferendumController {
+
+    @FXML
+    private CheckBox favorevoleCheck;
+
+    @FXML
+    private CheckBox sfavorevoleCheck;
+
+    @FXML
+    void vota(ActionEvent event) {
+
+    }
+
+}
